5c6052116ffcacd2c81cc68232fe8570b58fde78
Federico Rocha Williams <federiconrocha@gmail.com>
2021-08-04 22:03:56 -0300

    Clase 1

---
+++ Clase 01/01.intruduccion.js
@@ -0,0 +1,30 @@
+// var variable = "Algo";
+
+var fede = 'fede'
+
+if (fede === 'fede') {
+    // contexto, closed
+    // debugger;
+    var apellido = 'rocha';
+}
+
+let varibleQueSePuedeModificar = "";
+varibleQueSePuedeModificar = 222;
+
+const varibleQueNOSePuedeModificar = "asdasd";
+// varibleQueNOSePuedeModificar = 12312; // NO SE PUEDE
+
+const miObjeto = {
+    direccion: {
+        calle: 'Calle',
+        nro: 123
+    }
+}
+
+miObjeto.direccion = {};
+
+
+if (fede === 'fede') {
+    // contexto, closed
+    const apellidoDos = 'Williams';
+}
